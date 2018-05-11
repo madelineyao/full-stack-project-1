9e79e4a6aecde194b53b1cadd30817d0471b2f14
madelineyao <yao.madeline@gmail.com>
2018-05-11 14:47:55 -0800

    Made the quote appear randomly within certain time limit and make the background color change randomly

--- random-quote-v2/js/script.js
+++ random-quote-v2/js/script.js
@@ -2,7 +2,7 @@ var quotes = [
     {
       quote: 'One good thing about music, when it hits you, you feel no pain.', 
       source: 'Bob Marley', 
-      year: 1998       
+           
     }, 
     {
        quote:'Nothing is better than going home to family and eating good food and relaxing ', 
@@ -31,6 +31,11 @@ var quotes = [
 //create a number between 0 and the length of the quotes array
 //return the randomly picked quote object
 //
+function getQuoteWithinTimeInterval(){
+    var new_quote = setTimeout(getRandomQuote, 30000); 
+    return new_quote;
+}
+
 function getRandomQuote(){
     var number = Math.floor(Math.random()*quotes.length); 
     var random_quote = quotes[number]; 
@@ -40,7 +45,7 @@ function getRandomQuote(){
 
 
 function printQuote(){
-    var quote = getRandomQuote(); 
+    var quote = getQuoteWithinTimeInterval(); 
     var html = ''; 
     html += '<p class = "quote">'+ quote.quote + '</p>'; 
     html += '<p class = "source">'; 
@@ -53,7 +58,17 @@ function printQuote(){
         html += '<span class="year">'+quote.year + '</span>'; 
     }
     html+= '</p>'; 
+    randomColor();
     print(html); 
+}
+
+
+function randomColor(){
+    var x = Math.floor(Math.random()*256); 
+    var y = Math.floor(Math.random()*256); 
+    var z = Math.floor(Math.random()*256); 
+    var new_color = "rgb("+"x"+", "+"y"+", "+"z"+")";
+    document.body.style.backgroundColor = new_color;
 }
 
 
@@ -63,7 +78,27 @@ function print(message){
     div.innerHTML = message; 
 }
 
+/*
+1. Implement the color change function into the printQuote() 
+a. Write a seperate function for color change 
+ (1) Create the original three colors 
+ (2) Put them into rgb
+ (3) Use Window's property to set the background color 
 
+b. Put the function into printQuote()
+  (1) Put it before print(html)
+
+
+2. Implement the quote change into the getRandomQuote()
+a. Use getRandomQuote to make another function for the quote change within certain time interval
+  
+b. Put the new function into the PrintQuote()
+
+
+
+
+
+*/
 
 
 
